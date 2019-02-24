f87fae68bf9bf1ec30f8bd7f7f9ca1d5d829c90a
Adam Huang <adamhut@gmail.com>
2019-02-23 16:00:41 -0800

    Initial commit

---
+++ src/JokeFactory.php
@@ -0,0 +1,27 @@
+<?php 
+namespace Adamhut\ChuckNorrisJokes;
+
+
+class JokeFactory{
+
+    protected $jokes = [
+        'Chuck Norris counted to infinity... Twice',
+        'Chuck Norris\' tears cure cancer. Too bad he has never cried.',
+        'Chuck Norris does not wear a condom. Because there is no such thing as protection from Chuck Norris.',
+        'If you can see Chuck Norris, he can see you. If you can\'t see Chuck Norris you may be only seconds away from death'
+    ];
+
+    public function __construct(array $jokes = null)
+    {
+        if($jokes)
+        {
+            $this->jokes = $jokes;
+        }
+    }
+
+    public function getRandomJoke()
+    {
+        return $this->jokes[array_rand($this->jokes)];
+    }
+
+}
