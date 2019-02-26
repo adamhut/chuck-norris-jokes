9b0576bbcb887d0e9d99a84bfa0bdfb0c4be3443
Adam Huang <adamhut@gmail.com>
2019-02-26 09:10:46 -0800

    wip

--- src/JokeFactory.php
+++ src/JokeFactory.php
@@ -2,25 +2,37 @@
 
 namespace Adamhut\ChuckNorrisJokes;
 
+use GuzzleHttp\Client;
+
+
+
 class JokeFactory
 {
-    protected $jokes = [
-        'Chuck Norris counted to infinity... Twice',
-        'Chuck Norris\' tears cure cancer. Too bad he has never cried.',
-        'Chuck Norris does not wear a condom. Because there is no such thing as protection from Chuck Norris.',
-        'If you can see Chuck Norris, he can see you. If you can\'t see Chuck Norris you may be only seconds away from death',
-    ];
+    // protected $jokes = [
+    //     'Chuck Norris counted to infinity... Twice',
+    //     'Chuck Norris\' tears cure cancer. Too bad he has never cried.',
+    //     'Chuck Norris does not wear a condom. Because there is no such thing as protection from Chuck Norris.',
+    //     'If you can see Chuck Norris, he can see you. If you can\'t see Chuck Norris you may be only seconds away from death',
+    // ];
+    const  API_ENDPOINT = 'http://api.icndb.com/jokes/random';
 
-    public function __construct(array $jokes = null)
+    protected $client;
+
+    public function __construct(Client $client=null)
     {
-        if ($jokes) {
-            $this->jokes = $jokes;
-        }
+        $this->client = $client ?? new Client();
     }
 
     public function getRandomJoke()
     {
-        return $this->jokes[array_rand($this->jokes)];
+        $response = $this->client->request('GET',self::API_ENDPOINT);
+        
+        $t = $response->getBody()->getContents();
+        
+        $joke = json_decode($t);
+
+        return $joke->value->joke;
+        //var_dump( $response->getBody());
     }
 
     public function getRandomJokes($amount = 1)
@@ -33,3 +45,4 @@ class JokeFactory
         return $jokes;
     }
 }
+
--- tests/JokeFactoryTest.php
+++ tests/JokeFactoryTest.php
@@ -2,36 +2,65 @@
 
 namespace Adamhut\ChuckNorrisJokes\Tests;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7\Response;
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Exception\RequestException;
 use PHPUnit\Framework\TestCase;
 use Adamhut\ChuckNorrisJokes\JokeFactory;
 
 class JokeFactoryTest extends TestCase
 {
     /** @test */
-    public function it_returns_a_random_joke()
-    {
-        $jokes = new JokeFactory([
-            'This is a joke',
-        ]);
+    // public function it_returns_a_random_joke()
+    // {
+    //     $jokes = new JokeFactory([
+    //         'This is a joke',
+    //     ]);
 
-        $joke = $jokes->getRandomJoke();
+    //     $joke = $jokes->getRandomJoke();
 
-        $this->assertSame('This is a joke', $joke);
-    }
+    //     $this->assertSame('This is a joke', $joke);
+    // }
+
+    // /** @test */
+    // public function it_returns_a_predefined_joke()
+    // {
+    //     $predefinedJokes = [
+    //         'Chuck Norris counted to infinity... Twice',
+    //         'Chuck Norris\' tears cure cancer. Too bad he has never cried.',
+    //         'Chuck Norris does not wear a condom. Because there is no such thing as protection from Chuck Norris.',
+    //         'If you can see Chuck Norris, he can see you. If you can\'t see Chuck Norris you may be only seconds away from death',
+    //     ];
+
+    //     $jokes = new JokeFactory();
+
+    //     $joke = $jokes->getRandomJoke();
+    //     $this->assertContains($joke, $predefinedJokes);
+    // }
 
     /** @test */
-    public function it_returns_a_predefined_joke()
+    public function it_returns_another_random_joke()
     {
-        $predefinedJokes = [
-            'Chuck Norris counted to infinity... Twice',
-            'Chuck Norris\' tears cure cancer. Too bad he has never cried.',
-            'Chuck Norris does not wear a condom. Because there is no such thing as protection from Chuck Norris.',
-            'If you can see Chuck Norris, he can see you. If you can\'t see Chuck Norris you may be only seconds away from death',
-        ];
+        // Create a mock and queue two responses.
+        $mock = new MockHandler([
+            new Response(200, [], '
+{ "type": "success", "value": { "id": 398, "joke": "Chuck Norris is not Irish. His hair is soaked in the blood of his victims.", "categories": [] } }'),
+        ]);
 
-        $jokes = new JokeFactory();
+        $handler = HandlerStack::create($mock);
+
+        $client = new Client(['handler' => $handler]);
+
+        $jokes = new JokeFactory($client);
 
         $joke = $jokes->getRandomJoke();
-        $this->assertContains($joke, $predefinedJokes);
+
+        $this->assertSame( 'Chuck Norris is not Irish. His hair is soaked in the blood of his victims.', $joke);
+
     }
+
+    
 }
